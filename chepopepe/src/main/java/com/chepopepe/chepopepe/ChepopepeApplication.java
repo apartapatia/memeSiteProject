e3138314cccec2e6ceb59e41ad49332c9482a9c0
apartapatia <crsonylove@gmail.com>
2023-08-27 20:12:00 +0300

    first commit

---
+++ chepopepe/src/main/java/com/chepopepe/chepopepe/ChepopepeApplication.java
@@ -0,0 +1,13 @@
+package com.chepopepe.chepopepe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChepopepeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChepopepeApplication.class, args);
+	}
+
+}
